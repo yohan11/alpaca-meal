beb4bd90766929ce5e13f860e8f6024527d13c16
Junyeong Kim <jy.kim@alpacanetworks.com>
2025-02-04 14:53:48 +0900

    Implement showing restaurant

--- index.js
+++ index.js
@@ -1,31 +1,40 @@
-const lunchMenus = [
-    "김치찌개", "된장찌개", "순두부찌개", "부대찌개", "청국장찌개",
-    "제육볶음", "불고기", "닭갈비", "오징어볶음", "갈치조림",
-    "고등어구이", "삼겹살 정식", "수육국밥", "돼지국밥", "설렁탕",
-    "육개장", "감자탕", "찜닭", "낙지볶음", "콩나물국밥",
-    "김밥 + 라면", "떡볶이 + 순대", "잔치국수", "비빔국수", "칼국수",
-    "쫄면", "우동", "냉면", "막국수", "쌀국수",
-    "짜장면", "짬뽕", "볶음밥", "마파두부 덮밥", "깐풍기",
-    "초밥", "돈카츠", "규동", "라멘", "가라아게 정식",
-    "햄버거", "샌드위치", "파스타", "스테이크 덮밥", "리조또",
-    "타코", "케밥", "샐러드", "치킨", "피자"
-  ];
+import menuData from './data.json' with { type: "json" };
+
+
+const lunchMenus = menuData
+
+let link; // a 태그를 저장할 변수
+
+document.querySelector("button").addEventListener("click", () => {
+  const menuElement = document.querySelector(".menu");
+  const randomMenu = lunchMenus[Math.floor(Math.random() * lunchMenus.length)];
+
+  const menuDescriptionElement = document.querySelector(".menu_description");
+
+  menuElement.textContent = `오늘은 ${randomMenu.place_name} 가는 거 어때요?`;
+  menuDescriptionElement.textContent = `${randomMenu.place_name}은(는) ${randomMenu.category_name.split(">")[randomMenu.category_name.split(">").length - 1]}을 팔아요. 우리 회사에서 ${randomMenu.distance}m 떨어져 있네요^^ 걸어서 약 ${((randomMenu.distance / 5000) * 60).toFixed(0)}분 걸려요!`;
   
-  document.querySelector("button").addEventListener("click", () => {
-    const menuElement = document.querySelector(".menu");
-    const randomMenu = lunchMenus[Math.floor(Math.random() * lunchMenus.length)];
-    
-    menuElement.textContent = `오늘의 메뉴는 ${randomMenu}! 🍽️`;
-    menuElement.classList.add("show"); // 애니메이션 적용
+  menuElement.classList.add("show"); // 애니메이션 적용
+  menuDescriptionElement.classList.add("show"); // 애니메이션 적용
 
+  // 기존 링크가 있으면 제거
+  if (link) {
+    link.remove();
+  }
 
+  // 새로 링크 생성
+  link = document.createElement('a');
+  
+  // 링크에 속성 추가
+  link.href = randomMenu.place_url;  // 원하는 URL로 변경
+  link.textContent = "식당 정보가 궁금하다면?";  // 링크 텍스트
+  link.target="_blank"
+  // 링크 스타일링 (선택사항)
+  link.style.color = '#3674B5';
+  link.style.textDecoration = 'underline';
+  link.style.fontSize = '18px';
+  
+  // 페이지에 추가
+  document.body.appendChild(link);
+});
 
-  });
-
-  var container = document.getElementById('map');
-  var options = {
-      center: new kakao.maps.LatLng(33.450701, 126.570667),
-      level: 3
-  };
-
-  var map = new kakao.maps.Map(container, options);
